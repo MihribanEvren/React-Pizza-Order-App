6d9fe1e31017e76bc71d74d3b9127cb6bf901c31
Mihriban Evren <mihribanevrenn@gmail.com>
2024-09-02 07:18:14 +0300

    style: apply styling to the cart and button

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -6,6 +6,8 @@ function Button({ children, disabled, to, type }) {
 
   const styles = {
     primary: base + 'px-4 py-3 md:px-6 md:py-4',
+    secondary:
+      'inline-block border-2 border-stone-300 font-semibold tracking-wide uppercase transition-colors duration-200  rounded-full text-stone-400 hover:bg-stone-300 focus:outline-none focus:ring focus:ring-stone-200 focus:bg-stone-300 focus:ring-offset-2 disabled:cursor-not-allowed px-4 py-3.5 md:px-6 md:py-4 hover:text-stone-800 focus:text-stone-800',
     small: base + 'px-4 py-2 sm:px-5 sm:py-2.5 text-xs',
   };
 
